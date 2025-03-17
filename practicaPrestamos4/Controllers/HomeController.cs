e91911cf13ac39fc161e668ac7d6246a977ed1b0
Chellsaurius <altsella11@gmail.com>
2025-03-16 23:47:11 -0600

    ya funcionan los modales de prestamo

--- practicaPrestamos4/Controllers/HomeController.cs
+++ practicaPrestamos4/Controllers/HomeController.cs
@@ -13,17 +13,11 @@ namespace practicaPrestamos4.Controllers;
 [Authorize(Policy = "AuthenticatedUser")]  // Proteger todas las acciones del controlador
 
 
-public class HomeController : Controller
+public class HomeController(ILogger<HomeController> logger,
+    ApplicationDbContext context) : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-    private readonly ApplicationDbContext _context;
-
-    public HomeController(ILogger<HomeController> logger, 
-        ApplicationDbContext context)
-    {
-        _logger = logger;
-        _context = context;
-    }
+    private readonly ILogger<HomeController> _logger = logger;
+    private readonly ApplicationDbContext _context = context;
 
     [Authorize] // Solo usuarios autenticados pueden acceder a esta acción
     public IActionResult Index()
@@ -33,19 +27,20 @@ public class HomeController : Controller
         return View();
     }
 
-
+    [Authorize]
     public IActionResult Privacy()
     {
         return View();
     }
 
+    [Authorize]
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 
-    
+    [Authorize]
     // Método para redirigir al usuario a la URL de retorno o a la página principal
     private IActionResult RedirectToLocal(string returnUrl)
     {
